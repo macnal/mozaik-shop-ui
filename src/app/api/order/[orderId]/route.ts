af74d5595e642f2f5ca14a1458910e76fc15a26d
Eliasz <hauczak@hotmail.com>
2025-10-20 21:54:09 +0200

    cvb

--- src/app/api/order/[orderId]/route.ts
+++ src/app/api/order/[orderId]/route.ts
@@ -1,7 +1,11 @@
 import {NextRequest} from "next/server";
 import {WebLinkerService} from "@/services/weblinker";
 
-export async function GET(request: NextRequest, {params}: any) {
+interface GETRouteProps {
+  params: Promise<{ orderId: string }>,
+}
+
+export async function GET(request: NextRequest, {params}: GETRouteProps) {
   const {orderId} = await params;
 
   const dataSource = await WebLinkerService();
