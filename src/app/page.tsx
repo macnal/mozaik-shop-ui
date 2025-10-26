e093c3810e670ee6f2863d00bab4c5bef34d3c74
Maciej Nalewajko <maciej.nal@gmail.com>
2025-10-26 09:12:53 +0100

    naprawa paginacji na pierwszej stronie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,31 @@ import {WebLinkerService} from "@/services/weblinker";
 import {ItemsGrid} from "@/components/domain/ItemsGrid";
 import config from "@/../public/config.json";
 
-export default async function HomePage() {
-  const dataSource = await WebLinkerService();
-  // const res = await fetch(`${process.env.PUBLIC_URL}/config.json`);
-  // const config = await res.json();
+interface SearchPageProps {
+    params: Promise<{ categorySlug: string }>,
+    searchParams: Promise<{ page: string, size: string, query: string }>
+}
 
-  const r = dataSource.fetchProducts({
-    page: 0,
-    size: config?.interface?.itemsPerPage || 10,
-  });
-  const items = await r.then(({items}) => items);
-  const page = await r.then(({page}) => page);
+export default async function HomePage({params, searchParams}: SearchPageProps) {
+    const {page, query} = await searchParams;
+    const dataSource = await WebLinkerService();
+    // const res = await fetch(`${process.env.PUBLIC_URL}/config.json`);
+    // const config = await res.json();
 
-  return (
-    <PageContainer>
-      <ItemsGrid items={items} pagination={null}/>
+    const r = dataSource.fetchProducts({
+        page: isNaN(+page) ? 0 : +page,
+        size: config.interface.itemsPerPage,
+    });
+    const items = await r.then(({items}) => items);
+    /*
+      const page = await r.then(({page}) => page);
+    */
+    const pagination = await r.then(({page}) => page);
 
-    </PageContainer>
-  );
+    return (
+        <PageContainer>
+            <ItemsGrid items={items} pagination={pagination}/>
+
+        </PageContainer>
+    );
 }
